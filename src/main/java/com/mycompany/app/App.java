e2e1d75ad3f90128e47c53a71823af38d6738b4b
Scott Yang <yisyang@gmail.com>
2021-12-09 11:07:57 -0800

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,10 +5,9 @@ package com.mycompany.app;
  */
 public class App {
 
-  private final String message = "Hi World!";
+  private final String message = "Hello World!";
 
-  public App() {
-  }
+  public App() {}
 
   public static void main(String[] args) {
     System.out.println(new App().getMessage());
